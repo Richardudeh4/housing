37cc34a224fb2673ced689033f62969f256d983f
richardudeh4 <richardudeh03@gmail.com>
2024-12-08 17:56:33 +0100

    updated

--- components/Buying/page.tsx
+++ components/Buying/page.tsx
@@ -1,8 +1,14 @@
+import Image from 'next/image';
 import React from 'react'
+import background from "@/public/assets/backgroung.svg";
 
 const Buying = () => {
   return (
-    <div>Buying</div>
+    <div className="py-6 relative w-full">
+        <div className=''>
+
+        </div>
+    </div>
   )
 }
 
